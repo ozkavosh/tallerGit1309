7ef711d12afb6f0acb624b065e3370da6bf106dd
SantiagoRensonnet <63661270+SantiagoRensonnet@users.noreply.github.com>
2022-09-15 09:28:29 -0300

    modify functionality

--- Santi Nuevas Funcionalidades/santi1.js
+++ Santi Nuevas Funcionalidades/santi1.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById("sub_btn");
+const title = document.getElementById("title");
 const sub = document.getElementById("sub");
 
 const toggleSub = () => {
   sub.style.display = sub.style.display === "none" ? "block" : "none";
+  title.style.display = title.style.display === "none" ? "block" : "none";
 };
 
 btn.addEventListener("click", toggleSub);
